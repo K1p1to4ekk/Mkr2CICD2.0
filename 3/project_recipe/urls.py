5b3791aa715ab4dd3d4264d6bb32882c8b7646f1
Maxim Starostyn <maximstarostyn@gmail.com>
2025-05-21 12:25:12 +0300

    Delete 3 directory

--- 3/project_recipe/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from recipe import views
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.main, name='main'),
-]
